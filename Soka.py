d93ef4476905e92c0a1328e9868e7ee9484846c9
Xfya0 <154542535+Xfya0@users.noreply.github.com>
2024-04-21 22:07:19 +0300

    Update Soka.py

--- Soka.py
+++ Soka.py
@@ -1,4 +1,4 @@
-def convert_to_custom_format(character, write_side_by_side=False):
+def convert_to_custom_format(character):
     # تحويل الحرف/الحروف إلى صيغة النص المخصصة
     alphabet = {
         'a': ["  ___  ",
@@ -142,6 +142,16 @@ def convert_to_custom_format(character, write_side_by_side=False):
         if char == "\n":
             converted_characters.append("\n")
         elif char in alphabet:
-            if write_side_by_side:
-                converted_characters.extend(alphabet[char])
-                converted_characters.append(" ")  # إضافة
+            converted_characters.append(alphabet[char])
+        else:
+            converted_characters.append([""])
+
+    # دمج الصفوف
+    zipped_characters = list(zip(*converted_characters))
+
+    
+    # تحويل الصفوف المدمجة إلى سلاسل نص
+    lines = [” “.join(row) for row in zipped_characters]
+
+# دمج جميع السلاسل مع إضافة فواصل بين الأسطر لإنشاء النص النهائي
+return "\n".join(lines)
