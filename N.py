83e67f617d0e1333828ac337a8cf76244b39e744
Xfya0 <154542535+Xfya0@users.noreply.github.com>
2024-05-18 00:16:08 +0300

    Update N.py

--- N.py
+++ N.py
@@ -3,14 +3,13 @@ import nfc.clf
 
 def on_connect(tag):
     if tag.ndef:
-        text = nfc.ndef.TextRecord("Hello, NFC!")
+        text = nfc.ndef.TextRecord("Reusable NFC Card")
         ndef_message = nfc.ndef.Message(text)
         tag.ndef.message = ndef_message
         print("Message written to tag.")
-        return True
     else:
         print("Tag is not NDEF formatted.")
-        return False
+    return True
 
 with nfc.ContactlessFrontend('usb') as clf:
     clf.connect(rdwr={'on-connect': on_connect})
