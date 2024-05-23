d1d27929f5d01d21d2569c057d4bb1df294230f7
Xfya0 <154542535+Xfya0@users.noreply.github.com>
2024-05-23 18:09:06 +0300

    Update chek.py

--- chek.py
+++ chek.py
@@ -11,16 +11,20 @@ def check_username(username):
         response = requests.get(url, headers=headers)
         
         if response.status_code == 200:
+            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (مستخدم)")
             return f"اسم المستخدم {username} مستخدم."
         elif response.status_code == 404:
+            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (متاح)")
             return f"اسم المستخدم {username} متاح."
         else:
+            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (غير معروف)")
             return f"لم يتمكن من فحص اسم المستخدم {username}. الحالة: {response.status_code}"
     except requests.exceptions.RequestException as e:
+        print(f"خطأ في الاتصال لاسم المستخدم {username}: {e}")
         return f"حدث خطأ أثناء محاولة فحص اسم المستخدم {username}: {e}"
 
 def main():
-    with open('username.txt', 'r', encoding='utf-8') as file:
+    with open('usernames.txt', 'r', encoding='utf-8') as file:
         usernames = file.readlines()
     
     usernames = [username.strip() for username in usernames]
