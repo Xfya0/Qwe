d2ef07eda64f1562c8c43508da1435df65a8ff4f
Xfya0 <154542535+Xfya0@users.noreply.github.com>
2024-05-23 18:16:49 +0300

    Update chek.py

--- chek.py
+++ chek.py
@@ -11,28 +11,29 @@ def check_username(username):
         response = requests.get(url, headers=headers)
         
         if response.status_code == 200:
-            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (مستخدم)")
-            return f"اسم المستخدم {username} مستخدم."
+            return f"اسم المستخدم '{username}' مستخدم."
         elif response.status_code == 404:
-            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (متاح)")
-            return f"اسم المستخدم {username} متاح."
+            return f"اسم المستخدم '{username}' متاح."
         else:
-            print(f"رد الموقع لاسم المستخدم {username}: {response.status_code} (غير معروف)")
-            return f"لم يتمكن من فحص اسم المستخدم {username}. الحالة: {response.status_code}"
+            return f"لم يتمكن من فحص اسم المستخدم '{username}'. الحالة: {response.status_code}"
     except requests.exceptions.RequestException as e:
-        print(f"خطأ في الاتصال لاسم المستخدم {username}: {e}")
-        return f"حدث خطأ أثناء محاولة فحص اسم المستخدم {username}: {e}"
+        return f"حدث خطأ أثناء محاولة فحص اسم المستخدم '{username}': {e}"
 
 def main():
-    with open('username.txt', 'r', encoding='utf-8') as file:
+    with open('usernames.txt', 'r', encoding='utf-8') as file:
         usernames = file.readlines()
     
     usernames = [username.strip() for username in usernames]
     
+    results = []
     for username in usernames:
         result = check_username(username)
-        print(result)
+        results.append(result)
         time.sleep(2)  # الانتظار لمدة 2 ثانية بين الطلبات لتجنب الحظر
+    
+    with open('results.txt', 'w', encoding='utf-8') as result_file:
+        for result in results:
+            result_file.write(result + '\n')
 
 if __name__ == "__main__":
     main()
